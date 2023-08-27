23164b5e0c25eeea1556f68b28c7ade6e9ab6a70
Şule Celep <120751470+sulecelep@users.noreply.github.com>
2023-08-27 14:39:34 +0300

    Guest Class Added 87 Completed

---
+++ JwtProject/WebApiJwt/Controllers/DefaultController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiJwt.Models;
+
+namespace WebApiJwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult TokenOlustur()
+        {
+            return Ok(new CreateToken().TokenCreate());
+        }
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult TestToken()
+        {
+            return Ok("Hoş Geldiniz");
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult AdminTokenOlustur()
+        {
+            return Ok(new CreateToken().TokenCreateAdmin());
+        }
+        [Authorize(Roles ="Admin, Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult TestRole()
+        {
+            return Ok("Admin/Visitor Başarılı Şekilde Giriş Yaptı");
+        }
+    }
+}
---
+++ JwtProject/WebApiJwt/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Jwt configuration'ý
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt =>
+    {
+        opt.RequireHttpsMetadata = false;
+        opt.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidIssuer = "http://localhost",
+            ValidAudience = "http://localhost",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("aspnetcoreapiapi")),
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+            ClockSkew= TimeSpan.Zero
+        };
+    });
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication(); //jwt autheticaton için
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
