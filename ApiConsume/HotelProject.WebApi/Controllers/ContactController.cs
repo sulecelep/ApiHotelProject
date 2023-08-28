e3a21add3593e2ee6bd8354c9e966555d7e9a5c2
Şule Celep <120751470+sulecelep@users.noreply.github.com>
2023-08-28 23:06:17 +0300

    93 Completed

---
+++ ApiConsume/HotelProject.WebApi/Controllers/ContactController.cs
@@ -0,0 +1,52 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContactService _contactService;
+
+        public ContactController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        //[HttpGet]
+        //public IActionResult ContactList()
+        //{
+        //    var values = _contactService.TGetList();
+        //    return Ok(values);
+        //}
+        [HttpPost]
+        public IActionResult AddContact(Contact contact)
+        {
+            contact.Date= Convert.ToDateTime(DateTime.Now.ToString());  
+            _contactService.TInsert(contact);
+            return Ok();
+        }
+        //[HttpDelete("{id}")]
+        //public IActionResult DeleteContact(int id)
+        //{
+        //    var value = _contactService.TGetByID(id);
+        //    if (value != null)
+        //    {
+        //        _contactService.TDelete(value);
+        //        return Ok();
+
+        //    }
+        //    return BadRequest();
+        //}
+        //[HttpPut("UpdateContact")]
+        //public IActionResult UpdateContact(Contact contact)
+        //{
+        //    _contactService.TUpdate(contact);
+        //    return Ok();
+        //}
+        
+    }
+}
