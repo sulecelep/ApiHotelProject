d2e6d29eb33e1612695ad3eded454872ae69cee6
ssule <ssulecelepp@gmail.com>
2023-12-01 19:26:54 +0300

    28 Tamamlandı.

---
+++ Frontend/HotelProject.WebUI/Controllers/SettingsController.cs
@@ -0,0 +1,46 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Models.Setting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class SettingsController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public SettingsController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel viewModel = new UserEditViewModel();
+            viewModel.Name = user.Name; 
+            viewModel.Surname = user.Surname;
+            viewModel.Username = user.UserName;
+            viewModel.Email = user.Email;
+
+            return View(viewModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel model)
+        {
+            if(model.Password ==model.ConfirmPassword)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                user.Name = model.Name;
+                user.Surname = model.Surname;
+                user.Email = model.Email;
+                user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, model.Password);
+                await _userManager.UpdateAsync(user);
+                return RedirectToAction("Index", "Login");
+
+            }
+            return View();
+
+        }
+    }
+}
